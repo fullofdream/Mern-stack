ea8518cd662579e819809ebf716305febb36d0e8
fullofdream <satoshinji528@gmail.com>
2023-12-13 20:09:33 -0800

    updated successfully

--- Take_My_Stuff/TakeMyStuff/App.js
+++ Take_My_Stuff/TakeMyStuff/App.js
@@ -6,18 +6,28 @@ export default function App() {
   console.log('App executed..');
   return (
     <View style={styles.container}>
+
       <Text>Hello World!!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
+
   container: {
+
     flex: 1,
+
     backgroundColor: '#fff',
+
     alignItems: 'center',
+
     justifyContent: 'center',
+
   },
+
 });
 
